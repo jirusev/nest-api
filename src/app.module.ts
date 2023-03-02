fb7b4086081aa00935547a6a217ba16bbb679945
jivko rusev <zhivko@scscloud.com>
2023-03-02 18:47:34 +0200

    Import fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ProductModule } from './products/product.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, AuthModule, UsersModule],
+  imports: [ProductModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
